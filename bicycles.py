12eeaeb78145bebb6ae77f7c22395ce53451a7c4
Alice Xie <alice.xie@autodesk.com>
2015-11-05 05:32:33 +0000

    Bicycles Extra Challenges

--- bicycles.py
+++ bicycles.py
@@ -1,10 +1,27 @@
 import random
 
-class Bicycles(object):
-    def __init__(self, modelName, weight, cost):
-        self.modelName = modelName
+material_list=['aluminum', 'carbon', 'steel']
+
+class Wheel(object):
+    def __init__(self, name, weight, cost):
+        self.name = name
         self.weight = weight
         self.cost = cost
+    
+class Frame(object):
+    def __init__(self, material, weight, cost):
+        self.material = material
+        self.weight = weight
+        self.cost = cost
+        
+class Bicycles(object):
+    def __init__(self, name, wheel, frame):
+        self.name = name
+        self.front_wheel = wheel
+        self.back_wheel = wheel
+        self.frame = frame
+        self.weight = self.front_wheel.weight + self.back_wheel.weight + self.frame.weight
+        self.cost = self.front_wheel.cost + self.back_wheel.cost + self.frame.cost
     
 class BikeShop(object):
     def __init__(self, name, inventory, margin):
@@ -40,7 +57,7 @@ class BikeShop(object):
     
     def print_inventory(self):
         for bike in self.inventory.keys():
-            print(bike.modelName,self.inventory[bike])
+            print(bike.name,self.inventory[bike])
         
 class Customer(object):
     def __init__(self, name, budget):
@@ -54,13 +71,22 @@ class Customer(object):
         print("The left budget is {}".format(self.budget))
 
 def main():
-    #Create a bicycle shop that has 6 different bicycle models in stock. 
-    bike_model_A = Bicycles('model_A', 20, 100)
-    bike_model_B = Bicycles('Model_B', 18, 200)
-    bike_model_C = Bicycles('model_C', 22, 500)
-    bike_model_D = Bicycles('Model_D', 19, 800)
-    bike_model_E = Bicycles('model_E', 25, 1000)
-    bike_model_F = Bicycles('Model_F', 15, 1600)
+    
+    wheel_type_A = Wheel('wheel_A', 10, 50)
+    wheel_type_B = Wheel('wheel_B', 12, 200)
+    wheel_type_C = Wheel('wheel_C', 5, 400)
+    
+    frame_al = Frame(material_list[0], 20, 100)
+    frame_carbon = Frame(material_list[1], 15, 400)
+    frame_steel = Frame(material_list[2], 25, 50)
+    
+    #Create a bicycle shop that has 6 different bicycle models in stock.
+    bike_model_A = Bicycles('model_A', wheel_type_A, frame_steel)
+    bike_model_B = Bicycles('Model_B', wheel_type_A, frame_al)
+    bike_model_C = Bicycles('model_C', wheel_type_B, frame_al)
+    bike_model_D = Bicycles('Model_D', wheel_type_B, frame_carbon)
+    bike_model_E = Bicycles('model_E', wheel_type_C, frame_al)
+    bike_model_F = Bicycles('Model_F', wheel_type_C, frame_carbon)
     
     inventory = {   bike_model_A: 10,
                     bike_model_B: 10,
@@ -90,15 +116,15 @@ def main():
     
     print("Alice can afford these bikes:")
     for bike in alice_affordable_bikes:
-        print(bike.modelName)
+        print(bike.name)
         
     print("Jorge can afford these bikes:")
     for bike in jorge_affordable_bikes:
-        print(bike.modelName)
+        print(bike.name)
     
     print("Sooraj can afford these bikes: ")
     for bike in sooraj_affordable_bikes:
-        print(bike.modelName)
+        print(bike.name)
     
     print("Print the initial inventory of the bike shop for each bike it carries.")
     bikeshop.print_inventory()
@@ -107,21 +133,20 @@ def main():
     jorge_choice = random.choice(jorge_affordable_bikes)
     sooraj_choice = random.choice(sooraj_affordable_bikes)
     
-    print("Alice buy bike {}".format(alice_choice.modelName))
+    print("Alice buy bike {}".format(alice_choice.name))
     Alice.buy_bike(bikeshop.sell_bike(alice_choice))
     
-    print("Jorge buy bike {}".format(jorge_choice.modelName))
+    print("Jorge buy bike {}".format(jorge_choice.name))
     Jorge.buy_bike(bikeshop.sell_bike(jorge_choice))
     
-    print("Sooraj buy bike {}".format(sooraj_choice.modelName))
+    print("Sooraj buy bike {}".format(sooraj_choice.name))
     Sooraj.buy_bike(bikeshop.sell_bike(sooraj_choice))
     
     print("print out the bicycle shop's remaining inventory for each bike")
     bikeshop.print_inventory()
     
     print("Profit get from selling bikes for {} is {}".format(bikeshop.name, bikeshop.calc_profit()))
-    
-    
+
     
 if(__name__ == '__main__'):
     main()
